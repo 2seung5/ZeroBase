bba73c36235bb17f73fe111d3ce4b2c8a69afaf0
Seung Ho <dltmdgh0904@naver.com>
2024-04-08 14:54:25 +0900

    1,2

---
+++ src/Mini01.java
@@ -0,0 +1,13 @@
+//이승호
+public class Mini01 {
+    public static void main(String[] args) {
+        System.out.println("[구구단 출력]");
+        for (int i = 1; i <10; i++) {
+            for (int j = 1; j < 10; j++) {
+                System.out.print(String.format("%02d",j)+" x "+String.format("%02d",i)+" = "+String.format("%02d",i*j));
+                System.out.print("\t");
+            }
+            System.out.println();
+        }
+    }
+}
---
+++ src/Mini02.java
@@ -0,0 +1,18 @@
+//이승호
+import java.util.*;
+public class Mini02 {
+    public static void main(String[] args) {
+        int price;
+        int cash;
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("[캐시백 계산]");
+        System.out.print("결제 금액 입력해 주세요.(금액):");
+        price = sc.nextInt();
+        cash = price/10;
+        cash -= cash%100;
+        if(cash>300)
+            cash =300;
+        System.out.println("결제 금액은 "+price+"원이고, 캐시백은 "+cash+ "원입니다.");
+    }
+}
