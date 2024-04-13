3f5f61a57f4a213e76119c535cfbc406ec88f23b
Seung Ho <dltmdgh0904@naver.com>
2024-04-14 05:32:57 +0900

    가상선거 시뮬레이션 프로그램

--- src/Mini06.java
+++ src/Mini06.java
@@ -1,33 +1,62 @@
+import java.util.HashMap;
+import java.util.Random;
 import java.util.Scanner;
 
 //이승호
 public class Mini06 {
-    class Vote{
-        String [][] vote = new String[10][10];
-        public void printVote(int voteNum, String[] name, int cnt){
-            for (int i = 0; i<2; i++){
-                for (int j = 0; j<cnt; j++){
-                    vote[i][j] = "ㄱ딛ㄱㄷㄱ";
+
+
+        public static void printVote(int voteNum, String[] name, int cnt){
+            int[] result = new int[voteNum];
+            HashMap<Integer, String> map = new HashMap<Integer,String>();          //이름과 기호번호 매핑 -> 투표한 기호로 이름 꺼내주기 위해
+            for (int i = 1; i<= cnt; i++){
+               map.put(i,name[i-1]);
+            }
+            //System.out.println(map.get(1));
+            result = randomVote(voteNum, cnt);
+            int[] count = new int[cnt]; // 투표받은 수 카운트 해주는 배열
+            for (int i = 0; i<cnt; i++){
+                count[i] = 0;
+            }
+            //System.out.println(result.length);
+
+            for(int i = 0; i< result.length; i++) {
+                System.out.printf("[투표진행률] : %.2f%%, %d 명 투표 => %s\n", (double)(i+1)/voteNum*100, i+1, map.get(result[i]));
+                int tmp = result[i];
+                count[tmp-1] +=1;
+                for (int j = 0; j < cnt; j++){
+                    System.out.printf("[기호:%d] %s:\t%.2f%%\t\t(투표수: %d)\n", j+1,name[j],(double)count[j]/voteNum*100, count[j]);
                 }
+                System.out.println();
             }
-            for(int i = 0; i< voteNum; i++) {
-                System.out.printf("[투표진행률] : %.2f, %d 명 투표 => %s", (double)i/voteNum, i, name);
-            }
+
         }
-    }
+
+        public static int[] randomVote(int voteNum, int cnt){
+            int[] voteResult = new int[voteNum];
+            Random random = new Random();
+            for (int i = 0; i< voteNum; i++){
+                voteResult[i] = random.nextInt(cnt)+1;             //voteResult ->투표 선택한 기호를 담는 배열
+            }
+            return voteResult;
+        }
+
 
     public static void main(String[] args) {
-        int voteNum;
+        int voteNum;   //10~10000
         int cnt; //후보자 인원수
         String [] name = new String[10];
         Scanner sc = new Scanner(System.in);
-        System.out.println("총 진행할 투표수를 입력해 주세요. : ");
+        System.out.print("총 진행할 투표수를 입력해 주세요. : ");
         voteNum = sc.nextInt();
-        System.out.println("가상 선거를 진행할 후보자 인원을 입력해 주세요. : ");
+        System.out.print("가상 선거를 진행할 후보자 인원을 입력해 주세요. : ");  //2~10
         cnt = sc.nextInt();
-        for (int i =0; i<cnt; i++) {
-            System.out.println(i + "번째 후보자이름을 입력해 주세요. : ");
-            name[i] = sc.nextLine();
+        for (int i =1; i<=cnt; i++) {
+            System.out.print(i + "번째 후보자이름을 입력해 주세요. : ");
+            name[i-1] = sc.next();
         }
+        printVote(voteNum, name, cnt);
+
+
     }
 }
