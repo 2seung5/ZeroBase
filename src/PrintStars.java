a515dddb553d4f6588b5c676d40383ed74c1e915
Seung Ho <dltmdgh0904@naver.com>
2024-04-13 21:02:00 +0900

    6 선거 시뮬레이션 ㅇㅇ

---
+++ src/PrintStars.java
@@ -0,0 +1,59 @@
+public class PrintStars {
+    public static void main(String[] args) {
+       for(int i = 0; i<5; i++){
+            for (int j = 0; j<=i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        System.out.println();
+        System.out.println();
+
+        for (int i = 0; i< 5; i++) {
+            for (int j = 0; j < 5; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        for (int i = 0; i<5; i++){
+            for (int k = 5-i; k>0 ; k--){
+                System.out.print(" ");
+            }
+            for (int j = 0 ; j<=i; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        for (int i = 0; i<5; i++){
+            for (int j = 5-i; j>0; j--){
+                System.out.print(" ");
+            }
+            for (int k = 0; k< 2*i+1; k++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        for (int i = 0; i <=5/2; i++){
+            for (int j =0; j<5/2-i; j++){
+                System.out.print(" ");
+            }
+            for (int k= 0; k<(2*i+1); k++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        for (int i = 5/2; i>0; i--){
+            for (int j = 0; j<5/2+1-i; j++){
+                System.out.print(" ");
+            }
+            for (int k = 0; k<2*i-1; k++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+    }
+}
