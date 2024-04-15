a4cc6f49a0c8d19392fe09eb2cc203f2b65cc3b3
Seung Ho <dltmdgh0904@naver.com>
2024-04-16 01:47:06 +0900

    stack 자료구조 프로그래머스 / 백준 문제풀이

--- src/Stack/BaekJoon25556.java
+++ src/Stack/BaekJoon25556.java
@@ -1,5 +1,47 @@
 package Stack;
+import java.util.Scanner;
+import java.util.Stack;
 
 public class BaekJoon25556 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n;
+        n = sc.nextInt();
+        int[] arr = new int[n];
+        int check = 0;
+        for (int i = 0; i< arr.length; i++){
+            arr[i] = sc.nextInt();
+        }
 
+        Stack stack1 = new Stack();
+        Stack stack2 = new Stack();
+        Stack stack3 = new Stack();
+        Stack stack4 = new Stack();
+
+        for (int i =0; i< arr.length; i++){
+            if(stack1.empty() || arr[i] > (int)stack1.peek()){
+                stack1.push(arr[i]);
+            }
+            else if(stack2.empty() || arr[i] > (int)stack2.peek()){
+                stack2.push(arr[i]);
+            }
+            else if(stack3.empty() || arr[i] > (int)stack3.peek()){
+                stack3.push(arr[i]);
+            }
+            else if (stack4.empty() || arr[i] > (int)stack4.peek()){
+                stack4.push(arr[i]);
+            }
+            else {
+                check = -1;
+            }
+        }
+        if (check == -1){
+            System.out.println("NO");
+        }
+        else {
+            System.out.println("YES");
+        }
+
+
+    }
 }
---
+++ src/Stack/Exam2.java
@@ -0,0 +1,39 @@
+package Stack;
+import javax.swing.*;
+import java.util.*;
+public class Exam2 {
+
+    public static class Solution {
+        public int[] solution(int []arr) {
+
+            Stack<Integer> stack = new Stack<Integer>();
+            for(int i = 0; i < arr.length; i++){
+                if(i==0){
+                    stack.push(arr[i]);
+                }
+                else if(stack.peek()!=arr[i]){
+                    stack.push(arr[i]);
+                }
+            }
+            int[] answer = new int[stack.size()];
+
+            for(int i=0; i< stack.size(); i++){
+                answer[stack.size()-i-1] = stack.pop();
+            }
+            /*for(int i=stack.size()-1; i>=0; i--){
+                answer[i] = stack.pop();
+            }*/
+            return answer;
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1,1,3,3,4,1};
+        Solution solution = new Solution();
+
+        int arrRe [] = solution.solution(arr);
+        for (int i = 0; i < arrRe.length; i++){
+            System.out.println(arrRe[i]);
+        }
+    }
+}
