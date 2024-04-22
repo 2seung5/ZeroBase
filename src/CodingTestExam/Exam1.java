77e2f8c2154361c627839a20ff12516adc84c4b8
Seung Ho <dltmdgh0904@naver.com>
2024-04-22 23:15:36 +0900

    코딩테스트 연습문제 2개

---
+++ src/CodingTestExam/Exam1.java
@@ -0,0 +1,29 @@
+package CodingTestExam;
+
+import java.util.HashMap;
+import java.lang.String;
+
+public class Exam1 {
+    public static void main(String[] args) {
+       /* String s = "123";
+        int answer = 0;
+
+        s=s.replace("zero","0");
+        s=s.replace("one","1");
+        s=s.replace("two","2");
+        s=s.replace("three","3");
+        s=s.replace("four","4");
+        s=s.replace("five","5");
+        s=s.replace("six","6");
+        s=s.replace("seven","7");
+        s=s.replace("eight","8");
+        s=s.replace("nine","9");
+        answer = Integer.parseInt(s);
+        System.out.println(answer);*/
+        String s = "asdf";
+        String[] a = s.split("");
+        System.out.println(a[2]);
+
+
+    }
+}
---
+++ src/CodingTestExam/Exam2.java
@@ -0,0 +1,45 @@
+package CodingTestExam;
+
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.Stack;
+
+public class Exam2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n;
+        n=sc.nextInt();
+        String[] s= new String[n];
+        for (int i =0; i<n; i++){
+            s[i]= sc.next();
+        }
+        String[][] arr = new String[n][];
+        for (int i = 0; i<s.length; i++){
+            arr[i] = s[i].split("");
+        }
+
+
+        Stack stack = new Stack();
+        for (int i = 0; i < arr.length; i++){
+            for (int j = 0; j < arr[i].length; j++){
+               if (arr[i][j].equals("(")){
+                   stack.push("(");
+
+
+               }else if(arr[i][j].equals(")")  && !stack.isEmpty()){
+                   stack.pop();
+               }else{
+                   stack.push(")");
+                   break;
+               }
+            }
+
+            if (stack.isEmpty()){
+                System.out.println("YES");
+            }else{
+                System.out.println("NO");
+            }
+            stack.clear();
+        }
+    }
+}
