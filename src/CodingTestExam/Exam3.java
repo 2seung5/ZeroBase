27c2afbb69b178105f5ba11360b13eab1b6d536b
Seung Ho <dltmdgh0904@naver.com>
2024-04-23 02:23:49 +0900

    코딩테스트 연습문제 1일차 5번

---
+++ src/CodingTestExam/Exam3.java
@@ -0,0 +1,26 @@
+package CodingTestExam;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+
+public class Exam3 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n;
+        n = Integer.parseInt(br.readLine());
+
+        String s = br.readLine();
+        String[] arr = s.split(" ");
+        int target;
+        target = Integer.parseInt(br.readLine());
+        int cnt =0;
+        for (int i = 0; i <arr.length; i++){
+            if (Integer.parseInt(arr[i]) == target){
+                cnt++;
+            }
+        }
+        System.out.println(cnt);
+    }
+}
